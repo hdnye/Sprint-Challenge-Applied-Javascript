9c096470b46c8e3c39db4f32f1de868a6b168670
Heather Nye <heatherdnye@gmail.com>
2019-10-13 18:47:26 -0500

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(headOne) {
+
+// const header = document.createElement('header');
+const date = document.createElement('date');
+const title = document.createElement('h1');
+const span = document.createElement('span');
+
+header.classList.add('header');
+date.classList.add('date');
+title.classList.add('title');
+span.classList.add('temp');
+
+headOne.forEach( (item) => {
+
+    const newItem = document.createElement('div');
+    newItem.textContent = item;
+    div.appendChild(item);
+})
+
+header.appendChild(date);
+date.appendChild(title);
+title.appendChild(span);
+
+return header;  
+    
+};
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header(headOne));
